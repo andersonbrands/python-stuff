db78e747a27cf99b93da6eefdb3cba7922a8c779
Anderson Brandao <anderson.brands@gmail.com>
2022-09-02 22:33:08 -0300

    Add context manager stuff

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
+from contextlib import contextmanager
+
+
 def answer():
     return 42
+
+
+class ClassyContextManager:
+    def __init__(self, thing):
+        self.thing = thing
+
+    def __enter__(self):
+        return self.thing
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+@contextmanager
+def funky_context_manager(thing):
+    yield thing
--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,19 @@
-from main import answer
+from unittest.mock import Mock
+
+from main import ClassyContextManager, answer, funky_context_manager
 
 
 def test_answer():
     assert 42 == answer()
+
+
+def test_class_context_manager():
+    mock = Mock()
+    with ClassyContextManager(mock) as classy:
+        assert classy is mock
+
+
+def test_funky_context_manager():
+    mock = Mock()
+    with funky_context_manager(mock) as funky:
+        assert funky is mock
