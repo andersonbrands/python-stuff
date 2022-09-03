91b3929089a55511dc91abd6be247fab163487e6
Anderson Brandao <anderson.brands@gmail.com>
2022-09-02 22:28:42 -0300

    Add decorator stuff

--- src/main.py
+++ src/main.py
@@ -1,2 +1,50 @@
+from functools import update_wrapper, wraps
+
+
 def answer():
     return 42
+
+
+def basic_decorator(func):
+    def wrapper():
+        return func()
+
+    return wrapper
+
+
+def cool_decorator(func):
+    @wraps(func)
+    def wrapper():
+        return func()
+
+    return wrapper
+
+
+def cooler_decorator(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+def offset(offset_):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            return func(*args, **kwargs) + offset_
+
+        return wrapper
+
+    return decorator
+
+
+class PrintNameUpper:
+    def __init__(self, func):
+        update_wrapper(self, func)
+        self.func = func
+        self.name_upper = str(func.__name__).upper()
+
+    def __call__(self, *args, **kwargs):
+        print(self.name_upper)
+        return self.func(*args, **kwargs)
--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,73 @@
-from main import answer
+from main import (
+    PrintNameUpper,
+    answer,
+    basic_decorator,
+    cool_decorator,
+    cooler_decorator,
+    offset,
+)
 
 
 def test_answer():
     assert 42 == answer()
+
+
+def test_basic_decorator_changes_name_and_doc():
+    def bla():
+        """blabla"""
+        pass
+
+    assert "bla" == bla.__name__
+    assert "blabla" == bla.__doc__
+
+    bla = basic_decorator(bla)
+
+    assert "bla" != bla.__name__
+    assert "blabla" != bla.__doc__
+
+
+def test_cool_decorator_keeps_name_and_doc():
+    def bla():
+        """blabla"""
+        pass
+
+    assert "bla" == bla.__name__
+    assert "blabla" == bla.__doc__
+
+    bla = cool_decorator(bla)
+
+    assert "bla" == bla.__name__
+    assert "blabla" == bla.__doc__
+
+
+def test_cooler_decorator_decorates_functions_with_args_and_kwargs():
+    @cooler_decorator
+    def bla(a):
+        pass
+
+    @cooler_decorator
+    def ble(a, b):
+        pass
+
+    bla(1)
+    ble(1, b=2)
+
+
+def test_decorator_with_args():
+    @offset(42)
+    def bla(a):
+        return a
+
+    value = 5
+    assert (value + 42) == bla(value)
+
+
+def test_class_decorator(capsys):
+    @PrintNameUpper
+    def bla():
+        pass
+
+    bla()
+    out = capsys.readouterr().out
+    assert "BLA" in out
+    assert "bla" not in out
