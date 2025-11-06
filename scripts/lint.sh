d5cf46184de9ac779ae464abe004d70c5f5c20a5
Anderson Brandao <anderson.brands@gmail.com>
2025-11-05 23:15:26 -0300

    Update lint.sh

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,22 +1,29 @@
 #!/usr/bin/env bash
 
-if ! hash python3 &> /dev/null; then
-  echo "python3 not found."
-  exit 1
+
+function lint {
+  flake8 ./src ./tests
+  black ./src ./tests
+  isort ./src ./tests
+}
+
+
+if [[ -z "$VIRTUAL_ENV" ]]; then
+  if ! hash python3 &> /dev/null; then
+    echo "python3 not found."
+    exit 1
+  fi
+
+  VENV_DIR=$(mktemp -d -p /tmp venv-XXXXXX)
+  python3 -m venv "$VENV_DIR"
+  source "$VENV_DIR"/bin/activate
+
+  pip install --upgrade pip setuptools
+  pip install ".[dev]"
+
+  lint
+
+  deactivate
+else
+  lint
 fi
-
-VENV_DIR=$(mktemp -d -p /tmp venv-XXXXXX)
-
-python3 -m venv "$VENV_DIR"
-
-source "$VENV_DIR"/bin/activate
-
-pip install --upgrade pip setuptools
-
-pip install ".[dev]"
-
-flake8 .
-black .
-isort .
-
-deactivate
