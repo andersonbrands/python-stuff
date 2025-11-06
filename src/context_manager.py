e3600a3ef6aedf71c3296090a9f6445f79fb5831
Anderson Brandao <anderson.brands@gmail.com>
2025-11-05 22:45:37 -0300

    Context manager (#7)

---
+++ src/context_manager.py
@@ -0,0 +1,17 @@
+from contextlib import contextmanager
+
+
+class ClassyContextManager:
+    def __init__(self, thing):
+        self.thing = thing
+
+    def __enter__(self):
+        return self.thing
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
+
+@contextmanager
+def funky_context_manager(thing):
+    yield thing
